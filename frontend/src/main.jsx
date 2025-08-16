7b385d82b798272bea98f3c77da3b3a7c211b326
Parth Goyal <mail.parthgoyal@gmail.com>
2025-08-17 01:39:24 +0530

    Added vercel analytics

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './styles/index.css'
+import { Analytics } from '@vercel/analytics/react'
 
 class ErrorBoundary extends React.Component {
   constructor(props) {
@@ -66,6 +67,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ErrorBoundary>
       <App />
+      <Analytics />
     </ErrorBoundary>
   </React.StrictMode>
 )
